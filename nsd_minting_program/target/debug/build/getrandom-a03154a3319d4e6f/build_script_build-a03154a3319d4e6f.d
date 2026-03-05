f9d87b07a208bcfbb0662ef4072b1b4bb215ccce
Niall Harrington <88103838+GetOutOfThatGarden@users.noreply.github.com>
2026-03-05 13:53:51 +0700

    chore: remove untracked target and node_modules directories to save space

--- nsd_minting_program/target/debug/build/getrandom-a03154a3319d4e6f/build_script_build-a03154a3319d4e6f.d
+++
@@ -1,5 +0,0 @@
-/Users/user2/Developer/NSD/nsd_minting_program/target/debug/build/getrandom-a03154a3319d4e6f/build_script_build-a03154a3319d4e6f.d: /Users/user2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.4/build.rs
-
-/Users/user2/Developer/NSD/nsd_minting_program/target/debug/build/getrandom-a03154a3319d4e6f/build_script_build-a03154a3319d4e6f: /Users/user2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.4/build.rs
-
-/Users/user2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.4/build.rs:
--- nsd_minting_program/target/debug/deps/percentage-64f25adb2d50fc8a.d
+++
@@ -1,5 +0,0 @@
-/Users/user2/Developer/NSD/nsd_minting_program/target/debug/deps/percentage-64f25adb2d50fc8a.d: /Users/user2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/percentage-0.1.0/src/lib.rs
-
-/Users/user2/Developer/NSD/nsd_minting_program/target/debug/deps/libpercentage-64f25adb2d50fc8a.rmeta: /Users/user2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/percentage-0.1.0/src/lib.rs
-
-/Users/user2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/percentage-0.1.0/src/lib.rs:
